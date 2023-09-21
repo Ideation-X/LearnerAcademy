ba36f11a9fd612f4fe7317fc7044fe739f12e5a4
92Mohammad <m.e89571828@gamil.com>
2023-09-21 16:37:31 +0530

    Emad: student profile page created

--- public/js/profilepage.js
+++ public/js/profilepage.js
@@ -17,9 +17,19 @@ const downBtn = document.getElementById('down-arrow-btn')
 downBtn.addEventListener('click', openProfileDropdownMenu)
 
 function openProfilePage() {
+    const container = document.getElementById('profile-page')
+    if (container.style.display === 'none') {
+        container.style.display = 'block'
+
+    }
+    else {
+        container.style.display = 'none'
+
+    }
     
 
 }
-
+const container = document.getElementById('profile-page')
+console.log(container)
 const profile_btn = document.getElementById('profile-btn')
 profile_btn.addEventListener('click', openProfilePage)
