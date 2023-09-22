ded69bd826398e1a59e104196e7b1da1354a257d
92Mohammad <m.e89571828@gamil.com>
2023-09-22 15:57:20 +0530

    Emad: side baar content problem fixed

--- public/js/side-bar.js
+++ public/js/side-bar.js
@@ -69,3 +69,38 @@ $("#c-u").click(() => {
     $(".contact-us").show();
 
 });
+
+console.log('side baar .js')
+
+function goToMainPage() {
+    const divContainer = document.getElementsByClassName('go_back_functionallity')
+
+    //first convert the each container inside content div to array from HTML collection
+    const divContainerArray = Array.from(divContainer)
+    //now each child container dsiplay property should be none , when I am clicking on the go back button
+    divContainerArray.forEach(container => {
+        container.style.display = 'none'
+
+       
+    })
+
+
+}
+const goBack = document.getElementsByClassName('go-back-btn')
+const BtnArray = Array.from(goBack)
+
+BtnArray.forEach(goBackBtn => {
+    // console.log('this is inside for each loop ', element)
+    goBackBtn.addEventListener('click', goToMainPage)
+    
+
+});
+
+
+
+
+
+
+
+// goBackBtn.addEventListener('click', goBackOnTOMainPage)
+
