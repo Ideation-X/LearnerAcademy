30dd62fc7990ea78212472c58d36bf50725d821a
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-06 00:12:56 +0530

    update home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import performancePicture from "../../assets/performance.png";
-import profilePicture from "../../assets/profile.jpg";
-import "./home.css"
+import "./home.css";
 
 const Home = () => {
     return (
@@ -26,67 +25,6 @@ const Home = () => {
                         id="profile-back-btn"
                         class="fa-solid fa-xmark fa-lg"
                     ></i>
-
-                    <div class="profile-content-container">
-                        <div class="profile-picture-and-name">
-                            <div class="profile-image-section">
-                                <div class="image-container">
-                                    <img src={profilePicture} alt="" id="profile-image" />
-                                </div>
-                                <div>
-                                    <span>Student Name</span>
-                                </div>
-                            </div>
-
-                            <div class="student-id-section">
-                                <div class="id-value-container">
-                                    <span id="student-id">Student id : </span>
-                                    <span id="id-value">56</span>
-                                </div>
-                                <div class="class-value-container">
-                                    <span id="student-class">Class</span>
-                                    <span id="class-value">9th</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="student-details">
-                            <div class="upper-section">
-                                <h2>Profile details</h2>
-
-                                <div>
-                                    <span>Name</span>
-                                    <span id="student-name">Emad</span>
-                                </div>
-
-                                <div>
-                                    <span>Mobile No</span>
-                                    <span id="student-phone">5487500405</span>
-                                </div>
-
-                                <div>
-                                    <span>Email</span>
-                                    <span id="student-email">jsgjfzglf</span>
-                                </div>
-
-                                <div>
-                                    <span>Living City</span>
-                                    <span id="student-city">fhgkgk</span>
-                                </div>
-
-                                <div>
-                                    <span>Board</span>
-                                    <span id="student-board">up</span>
-                                </div>
-
-                                <div>
-                                    <span>Language</span>
-                                    <span id="student-language">English</span>
-                                </div>
-                            </div>
-
-                            <div class="down-section">downsection</div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
