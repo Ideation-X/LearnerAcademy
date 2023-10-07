ab5ef4044a1e02df9817abae28393a86e419f423
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-07 21:05:10 +0530

    Adding Courses Page 📚

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import React from "react";
+
+import "./courses.css";
 
 const Courses = () => {
-  return (
-    <div>
-      Courses Page
-    </div>
-  )
-}
+    return (
+        <div>
+            <div class="tags-content">
+                <h1 class="about">About</h1>
+                <p class="about-p">
+                    Hey students, welcome to our book store. you can download
+                    any book here in free of cost. You can find any book just
+                    browse with class and publications.
+                </p>
+                <h1 class="about">Browse by class :</h1>
+                <div class="class-tags">
+                    <div class="tag">
+                        <span class="class">Class-01</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-02</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-03</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-04</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-05</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-06</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-07</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-08</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-09</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-10</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-11</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">Class-12</span>
+                    </div>
+                </div>
 
-export default Courses
+                <h1 class="about">Browse by publications :</h1>
+                <div class="publication-tags class-tags">
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                    <div class="tag">
+                        <span class="class">NCERT</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Courses;
