375ad6ef3a57c2f8532198e5cbda5407a16e4959
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-10 23:20:06 +0530

    Adding footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <footer className='footer-div'>
+      <p>&copy; Copyright <b>Learner Academy</b> 2023. All Right Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
