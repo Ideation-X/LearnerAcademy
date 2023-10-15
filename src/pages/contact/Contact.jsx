155cefba72c728b285f295fd05d711c8e5ce791a
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-16 00:34:03 +0530

    Adding Headings

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import "./contact.css";
 
 const Contact = () => {
-  return (
-    <div>
-      Contact Page
-    </div>
-  )
-}
+    return (
+        <div className="contact-div">
+            <div className="heading">
+                <h1>Contact</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Excepturi velit cumque, quaerat at et veritatis vero id
+                    deserunt provident exercitationem iusto ut facere, error rem
+                    temporibus quis? Et dolor porro distinctio repellendus
+                </p>
+            </div>
+        </div>
+    );
+};
 
-export default Contact
+export default Contact;
